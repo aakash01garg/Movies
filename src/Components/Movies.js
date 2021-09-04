d07ddb0b4fefd08978e8787fd3697fca6f123d9c
Aakash Garg <aakashgarg01@gmail.com>
2021-09-04 14:55:16 +0530

    Lifecycle Methods Added

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -1,15 +1,23 @@
+import './Movies.css'
+import axios from 'axios';
 import React, { Component } from 'react'
-import { getMovies } from './getMovies'
 export default class Movies extends Component {
 
     constructor() {
         super();
         this.state = {
-            movies: getMovies(),
+            movies: [],
             searchText: "",
             currPage: 1,
             limit: 3
         }
+    }
+
+    async componentDidMount() {
+        let res = await axios.get('https://backend-react-movie.herokuapp.com/movies');
+        this.setState({
+            movies: res.data.movies
+        })
     }
 
     onDelete = (id) => {
@@ -74,94 +82,102 @@ export default class Movies extends Component {
         let si = (currPage - 1) * limit;
         let ei = si + limit - 1;
         filteredArr = filteredArr.slice(si, ei + 1);
-
         let count = 0;
         return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-3">
-                        Hello
+
+            (filteredArr.length === 0)
+                ?
+                <div className="spinner-container">
+                    <div className="spinner-border text-primary" role="status">
+                        <span className="visually-hidden">Loading...</span>
                     </div>
-                    <div className="col-9">
-                        <div className="input-group mb-3 mt-3">
-                            <input onChange={(e) => { this.setState({ searchText: e.target.value }) }} type="text" className="form-control" placeholder="Search Movie" aria-label="Username" aria-describedby="basic-addon1" />
+                </div>
+                :
+                <div className="container">
+                    <div className="row">
+                        <div className="col-3">
+                            Hello
                         </div>
-                        <input type="text" className="form-control mb-3" placeholder="Number of Items per Page" aria-label="Username" aria-describedby="basic-addon1" onChange={(e) => {
-                            if (e.target.value !== "") {
-                                this.setState({
-                                    limit: Number(e.target.value)
-                                })
-                            }
-                            else {
-                                this.setState({
-                                    limit: 3
-                                })
-                            }
-                        }} />
-                        <table className="table">
-                            <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">Title</th>
-                                    <th scope="col">Genre</th>
-                                    <th scope="col">
-                                        <i className="fas fa-sort-up" onClick={() => {
-                                            this.sorted("stock", "asc");
-                                        }} ></i>
-                                        Stock
-                                        <i className="fas fa-sort-down" onClick={() => {
-                                            this.sorted("stock", "dsc");
-                                        }}></i>
-                                    </th>
-                                    <th scope="col">
-                                        <i className="fas fa-sort-up" onClick={() => {
-                                            this.sorted("rental", "asc");
-                                        }}></i>
-                                        Rental
-                                        <i className="fas fa-sort-down" onClick={() => {
-                                            this.sorted("rental", "dsc");
-                                        }}></i>
-                                    </th>
-                                    <th scope="col">Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    filteredArr.map((movie) => {
-                                        return (
-                                            <tr key={movie._id}>
-                                                <th scope="row">{++count}</th>
-                                                <td>{movie.title}</td>
-                                                <td>{movie.genre.name}</td>
-                                                <td>{movie.numberInStock}</td>
-                                                <td>{movie.dailyRentalRate}</td>
-                                                <td><button type="button" className="btn btn-danger" onClick={
-                                                    () => {
-                                                        this.onDelete(movie._id);
-                                                    }
-                                                } >Delete</button></td>
-                                            </tr>
-                                        )
+                        <div className="col-9">
+                            <div className="input-group mb-3 mt-3">
+                                <input onChange={(e) => { this.setState({ searchText: e.target.value }) }} type="text" className="form-control" placeholder="Search Movie" aria-label="Username" aria-describedby="basic-addon1" />
+                            </div>
+                            <input type="text" className="form-control mb-3" placeholder="Number of Items per Page" aria-label="Username" aria-describedby="basic-addon1" onChange={(e) => {
+                                if (e.target.value !== "") {
+                                    this.setState({
+                                        limit: Number(e.target.value)
                                     })
                                 }
-                            </tbody>
-                        </table>
-                        <nav aria-label="Page navigation example">
-                            <ul className="pagination">
-                                {
-                                    pagesArr.map((number) => {
-                                        return (
-                                            <li key={number} className={number === currPage ? "page-item active" : "page-item"} onClick={() => {
-                                                this.setState({ currPage: number })
-                                            }} ><button className="page-link">{number}</button></li>
-                                        )
+                                else {
+                                    this.setState({
+                                        limit: 3
                                     })
                                 }
-                            </ul>
-                        </nav>
-                    </div>
-                </div >
-            </div>
+                            }} />
+                            <table className="table">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">Title</th>
+                                        <th scope="col">Genre</th>
+                                        <th scope="col">
+                                            <i className="fas fa-sort-up" onClick={() => {
+                                                this.sorted("stock", "asc");
+                                            }} ></i>
+                                            Stock
+                                            <i className="fas fa-sort-down" onClick={() => {
+                                                this.sorted("stock", "dsc");
+                                            }}></i>
+                                        </th>
+                                        <th scope="col">
+                                            <i className="fas fa-sort-up" onClick={() => {
+                                                this.sorted("rental", "asc");
+                                            }}></i>
+                                            Rental
+                                            <i className="fas fa-sort-down" onClick={() => {
+                                                this.sorted("rental", "dsc");
+                                            }}></i>
+                                        </th>
+                                        <th scope="col">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        filteredArr.map((movie) => {
+                                            return (
+                                                <tr key={movie._id}>
+                                                    <th scope="row">{++count}</th>
+                                                    <td>{movie.title}</td>
+                                                    <td>{movie.genre.name}</td>
+                                                    <td>{movie.numberInStock}</td>
+                                                    <td>{movie.dailyRentalRate}</td>
+                                                    <td><button type="button" className="btn btn-danger" onClick={
+                                                        () => {
+                                                            this.onDelete(movie._id);
+                                                        }
+                                                    } >Delete</button></td>
+                                                </tr>
+                                            )
+                                        })
+                                    }
+                                </tbody>
+                            </table>
+                            <nav aria-label="Page navigation example">
+                                <ul className="pagination">
+                                    {
+                                        pagesArr.map((number) => {
+                                            return (
+                                                <li key={number} className={number === currPage ? "page-item active" : "page-item"} onClick={() => {
+                                                    this.setState({ currPage: number })
+                                                }} ><button className="page-link">{number}</button></li>
+                                            )
+                                        })
+                                    }
+                                </ul>
+                            </nav>
+                        </div>
+                    </div >
+                </div>
         )
     }
 }
